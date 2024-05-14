7a33c1f4381cc79fe1ea983292645b2fe45fb54c
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-05-14 21:47:53 +0700

    update st.py

---
+++ lib/constants/demo_constant.py
@@ -0,0 +1,52 @@
+MPII_KEYPOINT_INDEXES = {
+  0: "right ankle",
+  1: "right knee",
+  2: "right hip", 
+  3: "left hip", 
+  4: "left knee", 
+  5: "left ankle",
+  6: "pelvis", 
+  7: "thorax", 
+  8: "upper neck", 
+  9: "head top", 
+  10: "right wrist",
+  11: "right elbow", 
+  12: "right shoulder", 
+  13: "left shoulder", 
+  14: "left elbow",
+  15: "left wrist"
+}
+
+SKELETON = {
+  "left_lower_leg": [0, 1], 
+  "left_thigh": [2, 1], 
+  "left_hip": [2, 6], 
+  "right_lower_leg": [5, 4],
+  "right_thigh": [3, 4],
+  "right_hip": [3, 6],
+  "torso": [6, 7], 
+  "neck": [7, 8],             #actually it's thorax - upper neck 
+  "head": [8, 9],
+  "right_forearm": [10, 11],
+  "right_upper_arm": [11, 12], 
+  "right_shoulder": [12, 7],
+  "left_forearm": [15, 14],
+  "left_upper_arm": [14, 13], 
+  "left_shoulder": [13, 7]
+}
+
+SQUAT_PART = ["left_lower_leg", "left_thigh", "right_lower_leg",
+              "right_thigh", "torso"]
+
+SQUAT_KEYPART = [
+  ["left_lower_leg", "left_thigh"],
+  ["right_lower_leg", "right_thigh"]
+]
+
+STAGE_ANGLE = [180, 170, 143, 90, 150, 180]
+
+COLOR = {
+  "red": [0, 0, 255],
+  "blue": [255, 0, 0],
+  "green": [0, 255, 0]
+}
--- st.py
+++ st.py
@@ -18,6 +18,7 @@ from lib.models.pose_hrnet import get_pose_net
 from lib.config import cfg
 from lib.core.function import get_final_preds
 from lib.utils.transforms import get_affine_transform
+from lib.constants import demo_constant
 
 CTX = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 DEBUG = True
@@ -25,58 +26,12 @@ cudnn.benchmark = cfg.CUDNN.BENCHMARK
 torch.backends.cudnn.deterministic = cfg.CUDNN.DETERMINISTIC
 torch.backends.cudnn.enabled = cfg.CUDNN.ENABLED
 
-MPII_KEYPOINT_INDEXES = {
-  0: "right ankle",
-  1: "right knee",
-  2: "right hip", 
-  3: "left hip", 
-  4: "left knee", 
-  5: "left ankle",
-  6: "pelvis", 
-  7: "thorax", 
-  8: "upper neck", 
-  9: "head top", 
-  10: "right wrist",
-  11: "right elbow", 
-  12: "right shoulder", 
-  13: "left shoulder", 
-  14: "left elbow",
-  15: "left wrist"
-}
-
-SKELETON = {
-  "left_lower_leg": [0, 1], 
-  "left_thigh": [1, 2], 
-  "left_hip": [2, 6], 
-  "right_lower_leg": [5, 4],
-  "right_thigh": [4, 3],
-  "right_hip": [3, 6],
-  "torso": [6, 7], 
-  "neck": [7, 8],             #actually it's thorax - upper neck 
-  "head": [8, 9],
-  "right_forearm": [10, 11],
-  "right_upper_arm": [11, 12], 
-  "right_shoulder": [12, 7],
-  "left_forearm": [15, 14],
-  "left_upper_arm": [14, 13], 
-  "left_shoulder": [13, 7]
-}
-
-SQUAT_PART = ["left_lower_leg", "left_thigh", "right_lower_leg",
-              "right_thigh", "torso"]
-
-SQUAT_KEYPART = [
-  ["left_lower_leg", "left_thigh"],
-  ["right_lower_leg", "right_thigh"]
-]
-
-NUM_KPTS = len(MPII_KEYPOINT_INDEXES.keys())
-
-COLOR = {
-  "red": [0, 0, 255],
-  "blue": [255, 0, 0],
-  "green": [0, 255, 0]
-}
+NUM_KPTS = len(demo_constant.MPII_KEYPOINT_INDEXES)
+SKELETON = demo_constant.SKELETON
+SQUAT_PART = demo_constant.SQUAT_PART
+SQUAT_KEYPART = demo_constant.SQUAT_KEYPART
+STAGE_ANGLE = demo_constant.STAGE_ANGLE
+COLOR = demo_constant.COLOR
 
 # function definition
 def cosine_similarity(a, b):
@@ -87,6 +42,27 @@ def angle_degree(a, b):
   return (np.arccos(cosine_similarity(a, b)) / 
           np.pi) * 180
 
+def cosine_similarity(a, b):
+  cos = ( np.dot(a, b) / 
+          (np.linalg.norm(a) * np.linalg.norm(b)) )
+  if cos < -1:
+    return -1
+  elif cos > 1:
+    return 1
+  else:
+    return cos
+
+
+def angle_degree(a, b):
+  angle = (np.arccos(cosine_similarity(a, b)) / 
+          np.pi) * 180
+  if angle > 180:
+    return 180.
+  if angle < 0:
+    return 0
+  else:
+    return angle
+
 # @parameter
 # part: contain 2 joint coordinate [[x0, y0], [x1, y1]]
 def return_vector(part_id, skeleton):
@@ -94,21 +70,26 @@ def return_vector(part_id, skeleton):
   point_b = skeleton[part_id[1]]
   return point_a - point_b
 
-def compart_keypart(ske_true, ske_pred, keyparts, threshold= 10):
-  assert ske_true.shape == (NUM_KPTS, 2)
+def is_in_range(num, range1, range2):
+  ceil = max(range1, range2)
+  floor = min(range1, range2)
+  return (num >= floor and num <= ceil)
+
+def find_pose_id(ske_pred, keyparts, stage):
   assert ske_pred.shape == (NUM_KPTS, 2)
-  part_flag = []
-  for part_a, part_b in keyparts:
-    true_part_a = return_vector(SKELETON[part_a], ske_true)
-    true_part_b = return_vector(SKELETON[part_b], ske_true)
-    pred_part_a = return_vector(SKELETON[part_a], ske_pred)
-    pred_part_b = return_vector(SKELETON[part_b], ske_pred)
+  sum = 0
+  for keypart in keyparts:
+    part_a, part_b = keypart
+    vec_a = return_vector(SKELETON[part_a], ske_pred)
+    vec_b = return_vector(SKELETON[part_b], ske_pred)
 
-    true_angle = angle_degree(true_part_a, true_part_b)
-    pred_angle = angle_degree(pred_part_a, pred_part_b)
+    pred_angle = angle_degree(vec_a, vec_b)
+    sum += pred_angle
 
-    part_flag.append(True if abs(true_angle - pred_angle) <= threshold else False)
-  return part_flag
+  sum /= len(keyparts)
+  for i in range(len(stage)-1):
+    if is_in_range(sum, stage[i], stage[i+1]):
+      return i
 
 def compare_skeleton(ske_true, ske_pred, excer_part, threshold= 10):
   assert ske_true.shape == (NUM_KPTS, 2)
@@ -225,7 +206,8 @@ def update_config(default_cfg, config_file):
   default_cfg.defrost()
   default_cfg.merge_from_file(config_file)
   default_cfg.freeze()
-        
+
+# streamlit cache    
 @st.cache_resource
 def load_yolo(version):
   return torch.hub.load('ultralytics/yolov5', version, _verbose=False)
@@ -264,7 +246,7 @@ def load_true_poses(path):
   imgs = [cv2.imread(os.path.join(img_path, p)) for p in img_paths]
   skes = pd.read_csv(joints_path).to_numpy().reshape(len(imgs), NUM_KPTS, 2)
   return imgs, skes
-  
+
 def stop_processing():
   st.session_state["start"] = False
   if video_cap:
@@ -275,7 +257,9 @@ def start_processing():
   st.session_state["start"] = True
 
 # model loading and state init
-st.session_state["start"] = False
+if "start" not in st.session_state:
+  st.session_state["start"] = False
+
 box_model = load_yolo("yolov5s")
 pose_model = load_hrnet("./demo/w32_256x256_mpii.yaml")
 true_pose_images, true_pose_skes = load_true_poses("./squat")
@@ -287,16 +271,15 @@ model_selectbox = st.sidebar.selectbox("Model",
 )
 box_model_threshold = st.sidebar.number_input("Detetor threshold", min_value= 0.1, max_value=1., value=0.7)
 angle_threshold = st.sidebar.number_input("Angle threshold", min_value= 0, max_value=180, value= 5)
+true_pose_threshold = st.sidebar.number_input("True pose threshold", min_value= 0, max_value=100, value= 75)
 video_data = st.sidebar.file_uploader("Choose video", type=["mp4"])
 temp_video = "./temp.mp4"
 video_cap = None
 
 
 # main page
-col1, col2 = st.columns(3)
+col1, col2 = st.columns(2)
 col1.title("Your pose")
-placeholder = col1.empty()
-rep_holder = col1.empty()
 video_window = col1.image([])
 
 col2.title("True pose")
@@ -306,7 +289,6 @@ true_pose_window = col2.image([])
 if video_data and len(true_pose_images) and len(true_pose_skes):
   count = 0
   st.sidebar.button("Start", on_click= start_processing)
-  cancel_holder = st.empty()
 
   if st.session_state["start"]:
     st.sidebar.button("Cancel Execution", on_click= stop_processing)
@@ -315,14 +297,12 @@ if video_data and len(true_pose_images) and len(true_pose_skes):
       outfile.write(video_data.getbuffer())
     video_cap = cv2.VideoCapture(temp_video)
     while True:
-      pose_id = count % len(true_pose_images)
+      pose_id = 0
       rep = count // len(true_pose_images)
-      rep_holder.empty()
-      rep_holder.write(f"count: {count}")
       
       ret, image_bgr = video_cap.read()
       if ret:
-        true_pose_window.image(cv2.cvtColor(true_pose_images[pose_id], cv2.COLOR_BGR2RGB))
+        # true_pose_window.image(cv2.cvtColor(true_pose_images[pose_id], cv2.COLOR_BGR2RGB))
         last_time = time.time()
         image = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
         pred_boxes = get_person_detection_boxes(box_model, image, threshold= box_model_threshold)
@@ -332,10 +312,13 @@ if video_data and len(true_pose_images) and len(true_pose_skes):
           image_pose = image.copy() if cfg.DATASET.COLOR_RGB else image_bgr.copy()
           pose_preds = get_pose_estimation_prediction(pose_model, image_pose, center, scale)
           for skeleton in pose_preds:
+            pose_id = find_pose_id(skeleton, SQUAT_KEYPART, STAGE_ANGLE)
             true_skeleton = true_pose_skes[pose_id]
+            print(pose_id)
             part_flag = compare_skeleton(true_skeleton, skeleton, SQUAT_PART, threshold= angle_threshold)
             draw_pose(skeleton, part_flag, SQUAT_PART, image_bgr)
-            if False not in part_flag:
+            true_percent = sum(part_flag) / len(part_flag)
+            if true_percent >= true_pose_threshold:
               count += 1
 
         if DEBUG:
@@ -343,6 +326,7 @@ if video_data and len(true_pose_images) and len(true_pose_skes):
           image = cv2.putText(image_bgr, 'fps: '+ "%.2f"%(fps), (25, 40), 
                               cv2.FONT_HERSHEY_SIMPLEX, 1.2, COLOR["blue"], 2)
 
+        true_pose_window.image(cv2.cvtColor(true_pose_images[pose_id], cv2.COLOR_BGR2RGB))
         result_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         video_window.image(result_image)
       else:
